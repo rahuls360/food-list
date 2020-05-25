d05de20f9a88c410ec91e375939cb19957da66e3
Rahul Makhija <rahuls360@gmail.com>
2020-05-25 17:25:15 +0530

    add button for adding food item

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert, Modal, TouchableHighlight, TextInput, Picker } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  Modal,
+  TouchableHighlight,
+  TextInput,
+  Picker,
+  Button,
+} from 'react-native';
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
 const typesArray = ['Breakfast', 'Lunch', 'Snack', 'Dinner'];
@@ -17,70 +28,78 @@ export default function App() {
     ['Palak Paneer', 'Dinner', 'palak-paneer'],
     ['Sambosa', 'Snack', 'sambosa'],
   ]);
-  
- 
+
   const _alertIndex = (id, action) => {
     // Alert.alert(`${action}ing ${id}`);
-    
-    if(action  === 'Delete'){
+
+    if (action === 'Delete') {
       Alert.alert(
         `Are you sure you want to delete ${id}`,
-        "",
+        '',
         [
           {
-            text: "Cancel",
-            onPress: () => console.log("Cancel Pressed"),
-            style: "cancel"
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
           },
-          { text: "OK", onPress: () => {
-            const data = tableData.filter(item => item[2] !== id);
-            console.log("OK Pressed", data);
-            setTableData(data)
-          } }
+          {
+            text: 'OK',
+            onPress: () => {
+              const data = tableData.filter((item) => item[2] !== id);
+              console.log('OK Pressed', data);
+              setTableData(data);
+            },
+          },
         ],
-        { cancelable: false }
+        { cancelable: false },
       );
-    }else {
+    } else if (action === 'Edit') {
       setModalData({
-      visible: true,
+        visible: true,
         id,
       });
-      const editingRow = tableData.find(item => item[2] === id);
+      const editingRow = tableData.find((item) => item[2] === id);
       setName(editingRow[0]);
       setType(editingRow[1]);
+    } else {
+      // Add
+      console.log(new Date());
     }
-  }
-      const element = (data, index) => (
-        <View style={styles.rowSpace}>
-        <TouchableOpacity onPress={() => _alertIndex(data, "Edit")}>
-          <View style={styles.btn}>
-            <Text style={styles.btnText}>Edit</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => _alertIndex(data, "Delete")}>
-          <View style={styles.btn}>
-            <Text style={styles.btnText}>Delete</Text>
-          </View>
-        </TouchableOpacity>
+  };
+  const element = (data, index) => (
+    <View style={styles.rowSpace}>
+      <TouchableOpacity onPress={() => _alertIndex(data, 'Edit')}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>Edit</Text>
         </View>
-      );
-    return (
-      <>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => _alertIndex(data, 'Delete')}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>Delete</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+  return (
+    <>
       <View style={styles.container}>
-         <Table borderStyle={{borderColor: 'transparent'}}>
-            <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
-            {
-              tableData.map((rowData, index) => (
-                <TableWrapper key={index} style={styles.row}>
-                  {
-                    rowData.map((cellData, cellIndex) => (
-                      <Cell key={cellIndex} data={cellIndex === 2 ? element(cellData, index) : cellData} textStyle={styles.text}/>
-                    ))
-                  }
-                </TableWrapper>
-              ))
-            }
-          </Table>
+        <View style={styles.bottomPadding}>
+          <Button title="Add" onPress={() => _alertIndex(null, 'Add')} />
+        </View>
+        <Table borderStyle={{ borderColor: 'transparent' }}>
+          <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+          {tableData.map((rowData, index) => (
+            <TableWrapper key={index} style={styles.row}>
+              {rowData.map((cellData, cellIndex) => (
+                <Cell
+                  key={cellIndex}
+                  data={cellIndex === 2 ? element(cellData, index) : cellData}
+                  textStyle={styles.text}
+                />
+              ))}
+            </TableWrapper>
+          ))}
+        </Table>
       </View>
       <View style={styles.centeredView}>
         <Modal
@@ -88,71 +107,71 @@ export default function App() {
           transparent={true}
           visible={modalData.visible}
           onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
+            Alert.alert('Modal has been closed.');
           }}
-          placeholder='Enter Food Name'
+          placeholder="Enter Food Name"
         >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-        <Text>Editing - {modalData.id}</Text>
-        <TextInput
-        style={styles.centerText}
-          onChangeText={text => setName(text)}
-          value={name}
-        />
-        <Picker
-                  selectedValue={type}
-                  style={{ height: 50, width: 150 }}
-                  placeholder='Enter Food Type'
-                  onValueChange={(itemValue, itemIndex) => setType(itemValue)}>
-                  {typesArray.map(item => (
-                    <Picker.Item label={item} value={item} key={item}/>
-                  ))}
-        </Picker>
-            <TouchableHighlight
-              style={{ backgroundColor: "red" }}
-              onPress={() => {
-                setModalData({
-                  visible: !modalData.visible,
-                  id: ''
-                });
-                setName('');
-                setType('');
-              }}
-            >
-              <Text style={styles.textStyle}>Cancel</Text>
-            </TouchableHighlight>
-            <TouchableHighlight
-              style={{ backgroundColor: "green" }}
-              onPress={() => {
-                let foundIndex;
-                const editingRow = tableData.find((item, index) => {
-                  foundIndex = index;
-                  return item[2] === modalData.id;
-                });
-                editingRow[0] = name;
-                editingRow[1] = type;
-                setModalData({
-                  visible: !modalData.visible,
-                  id: ''
-                });
-                setName('');
-                setType('');
-                const newTableData = [...tableData];
-                newTableData[foundIndex] = editingRow;
-                setTableData(newTableData);
-              }}
-            >
-              <Text style={styles.textStyle}>Save Changes</Text>
-            </TouchableHighlight>
-          </View>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text>Editing - {modalData.id}</Text>
+              <TextInput
+                style={styles.centerText}
+                onChangeText={(text) => setName(text)}
+                value={name}
+              />
+              <Picker
+                selectedValue={type}
+                style={{ height: 50, width: 150 }}
+                placeholder="Enter Food Type"
+                onValueChange={(itemValue, itemIndex) => setType(itemValue)}
+              >
+                {typesArray.map((item) => (
+                  <Picker.Item label={item} value={item} key={item} />
+                ))}
+              </Picker>
+              <TouchableHighlight
+                style={{ backgroundColor: 'red' }}
+                onPress={() => {
+                  setModalData({
+                    visible: !modalData.visible,
+                    id: '',
+                  });
+                  setName('');
+                  setType('');
+                }}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={{ backgroundColor: 'green' }}
+                onPress={() => {
+                  let foundIndex;
+                  const editingRow = tableData.find((item, index) => {
+                    foundIndex = index;
+                    return item[2] === modalData.id;
+                  });
+                  editingRow[0] = name;
+                  editingRow[1] = type;
+                  setModalData({
+                    visible: !modalData.visible,
+                    id: '',
+                  });
+                  setName('');
+                  setType('');
+                  const newTableData = [...tableData];
+                  newTableData[foundIndex] = editingRow;
+                  setTableData(newTableData);
+                }}
+              >
+                <Text style={styles.textStyle}>Save Changes</Text>
+              </TouchableHighlight>
+            </View>
           </View>
         </Modal>
       </View>
-      </>
-    );
-
-  }
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
@@ -160,30 +179,39 @@ const styles = StyleSheet.create({
   text: { margin: 6 },
   row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
   rowSpace: { flexDirection: 'row', backgroundColor: '#fff' },
-  btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2, marginRight: 8 },
+  btn: {
+    width: 58,
+    height: 18,
+    backgroundColor: '#78B7BB',
+    borderRadius: 2,
+    marginRight: 8,
+  },
   btnText: { textAlign: 'center', color: '#fff' },
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
   },
   modalView: {
     margin: 20,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
   centerText: {
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
+  bottomPadding: {
+    marginBottom: 20,
+  },
 });
